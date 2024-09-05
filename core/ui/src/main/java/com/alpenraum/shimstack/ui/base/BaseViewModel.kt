3cbd4fbad98ca5a7176274ea1c9a39dd479a3507
Finn Zimmer <finn.zimmer@bawaggroup.com>
2024-09-05 14:53:23 +0200

    declarative approach on Bike Details screen

--- core/ui/src/main/java/com/alpenraum/shimstack/ui/base/BaseViewModel.kt
+++ core/ui/src/main/java/com/alpenraum/shimstack/ui/base/BaseViewModel.kt
@@ -6,6 +6,9 @@ import com.alpenraum.shimstack.common.DispatchersProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
 
 abstract class BaseViewModel(
     protected val dispatchersProvider: DispatchersProvider
@@ -27,4 +30,7 @@ abstract class BaseViewModel(
     open fun onStart() {}
 
     open fun onStop() {}
+
+    protected fun <T> Flow<T>.inViewModelScope(initialValue: T) =
+        this.stateIn(viewModelScope, started = SharingStarted.WhileSubscribed(), initialValue = initialValue)
 }
